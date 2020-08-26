61e4f665515bc4da2182677695ea7b75620385bc
mic_anghileri <micheleanghileri@gmail.com>
2020-08-26 17:07:32 +0200

    studenti stampati console log

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-// 3- Creare un array di oggetti di studenti.
-
-// 4-Ciclare su tutti gli studenti e stampare per ognuno nome e cognome.
-
-// 5- Dare la possibilità all’utente attraverso 3 prompt di aggiungere
-// un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
-// Usate prima i console.log e poi provare a stampare con jQuery.
-
 // 6- BONUS: Provate ad utilizzare Handlebars.
 
 $(document).ready(function(){
@@ -22,4 +14,51 @@ $(document).ready(function(){
 	for (var key in studente){
 		console.log(`${key}: ${studente[key]}`);
 	}
+
+    // 3- Creare un array di oggetti di studenti.
+
+    var studentiClasse = [
+        {
+            "nome": 'Michele',
+            "cognome": 'Anghileri',
+            "eta": 23
+        },
+        {
+            "nome": 'Robin',
+            "cognome": 'Hood',
+            "eta": 30
+        },
+        {
+            "nome": 'Christopher',
+            "cognome": 'McCandless',
+            "eta": 23
+        },
+        {
+            "nome": 'Ascanio',
+            "cognome": 'Celestini',
+            "eta": 48
+        },
+        {
+            "nome": 'Aboubakar',
+            "cognome": 'Soumahoro',
+            "eta": 40
+        },
+        {
+            "nome": 'Maso',
+            "cognome": 'Notarianni',
+            "eta": 53
+        }
+    ]
+
+    // 4-Ciclare su tutti gli studenti e stampare per ognuno nome e cognome.
+
+    for (var i = 0; i < studentiClasse.length; i++){
+        for (var key in studentiClasse[i]) {
+            console.log(`${key}: ${studentiClasse[i][key]}`);
+        }
+    }
+
+    // 5- Dare la possibilità all’utente attraverso 3 prompt di aggiungere
+    // un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+    // Usate prima i console.log e poi provare a stampare con jQuery.
 });
